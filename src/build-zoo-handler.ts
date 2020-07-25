cd7a687181d206496f26a27005126ebf4b143d60
Janne Valkealahti <janne.valkealahti@gmail.com>
2020-07-25 16:23:52 +0100

    Basic feature to branch/tag release
    - Relates #5

--- src/build-zoo-handler.ts
+++ src/build-zoo-handler.ts
@@ -2,16 +2,29 @@ import * as core from '@actions/core';
 import {checkCommands} from './ensure-commands';
 import {checkEnv} from './ensure-env';
 import {splitStringToArray} from './utils';
+import {tagRelease} from './tag-release';
 
 async function run() {
   try {
+    // ensure commands
     const ensureCommands = core.getInput('ensure-commands', {required: false});
     if (ensureCommands) {
       await checkCommands(splitStringToArray(ensureCommands));
     }
+
+    // ensure environment variables
     const ensureEnv = core.getInput('ensure-env', {required: false});
     if (ensureEnv) {
       await checkEnv(splitStringToArray(ensureEnv));
+    }
+
+    // tag release
+    const username = core.getInput('tag-release-username', {required: false});
+    const useremail = core.getInput('tag-release-useremail', {required: false});
+    const branch = core.getInput('tag-release-branch', {required: false});
+    const tag = core.getInput('tag-release-tag', {required: false});
+    if (username && useremail && branch && tag) {
+      await tagRelease(username, useremail, branch, tag);
     }
   } catch (error) {
     core.setFailed(error.message);
---
+++ src/tag-release.ts
@@ -0,0 +1,24 @@
+import * as core from '@actions/core';
+import {exec} from '@actions/exec';
+
+export async function tagRelease(
+  username: string,
+  useremail: string,
+  branch: string,
+  tag: string
+): Promise<void> {
+  await runCli(['config', '--global', 'user.name', username]);
+  await runCli(['config', '--global', 'user.email', useremail]);
+  await runCli(['checkout', '-b', branch]);
+  await runCli(['commit', '-a', '-m', `Release ${branch}`]);
+  await runCli(['push', 'origin', `Release ${branch}`]);
+  await runCli(['tag', `v${tag}`]);
+  await runCli(['push', '--tags', 'origin']);
+}
+
+async function runCli(args: string[] | undefined) {
+  let res: number = await exec('git', args);
+  if (res !== core.ExitCode.Success) {
+    throw new Error('git exited with exit code ' + res);
+  }
+}
