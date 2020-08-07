1b49841f450782e30691313895b2302b5595b6ff
Janne Valkealahti <janne.valkealahti@gmail.com>
2020-08-07 17:24:41 +0100

    Polish

--- src/build-zoo-handler.ts
+++ src/build-zoo-handler.ts
@@ -1,15 +1,21 @@
 import * as core from '@actions/core';
-import {inspect} from 'util'
+import {inspect} from 'util';
 import {checkCommands} from './ensure-commands';
 import {checkEnv} from './ensure-env';
 import {tagRelease} from './tag-release';
 import {commitChanges} from './commit-changes';
 import {getPackCli} from './pack';
-import {handle, handleRepositoryDispatch, ClientPayloadData, extractContextProperties} from './dispatch-handler';
+import {
+  handle,
+  handleRepositoryDispatch,
+  ClientPayloadData,
+  extractContextProperties
+} from './dispatch-handler';
 import {splitStringToArray} from './utils';
 
 const DEFAULT_USERNAME = 'github-actions[bot]';
-const DEFAULT_USEREMAIL = '41898282+github-actions[bot]@users.noreply.github.com';
+const DEFAULT_USEREMAIL =
+  '41898282+github-actions[bot]@users.noreply.github.com';
 
 async function run() {
   try {
@@ -53,13 +59,26 @@ async function run() {
     }
 
     // commit changes
-    const commitUsername = core.getInput('commit-changes-username', {required: false}) || DEFAULT_USERNAME;
-    const commitUseremail = core.getInput('commit-changes-useremail', {required: false}) || DEFAULT_USEREMAIL;
-    const commitBranch = core.getInput('commit-changes-branch', {required: false});
-    const commitMessage = core.getInput('commit-changes-message', {required: false});
+    const commitUsername =
+      core.getInput('commit-changes-username', {required: false}) ||
+      DEFAULT_USERNAME;
+    const commitUseremail =
+      core.getInput('commit-changes-useremail', {required: false}) ||
+      DEFAULT_USEREMAIL;
+    const commitBranch = core.getInput('commit-changes-branch', {
+      required: false
+    });
+    const commitMessage = core.getInput('commit-changes-message', {
+      required: false
+    });
     if (commitBranch && commitMessage) {
       core.startGroup('Commit Changes Feature');
-      await commitChanges(commitUsername, commitUseremail, commitBranch, commitMessage);
+      await commitChanges(
+        commitUsername,
+        commitUseremail,
+        commitBranch,
+        commitMessage
+      );
       core.endGroup();
     }
 
@@ -75,18 +94,29 @@ async function run() {
     const dispatchHandlerToken = inputNotRequired('dispatch-handler-token');
     const dispatchHandlerOwner = inputNotRequired('dispatch-handler-owner');
     const dispatchHandlerRepo = inputNotRequired('dispatch-handler-repo');
-    const dispatchHandlerEventType = inputNotRequired('dispatch-handler-event-type') || 'build-zoo-handler';
+    const dispatchHandlerEventType =
+      inputNotRequired('dispatch-handler-event-type') || 'build-zoo-handler';
     const dispatchHandlerConfig = inputNotRequired('dispatch-handler-config');
-    const dispatchHandlerMax = Number(inputNotRequired('dispatch-handler-max') || "10");
-    const dispatchHandlerClientPayloadData = inputNotRequired('dispatch-handler-client-payload-data');
+    const dispatchHandlerMax = Number(
+      inputNotRequired('dispatch-handler-max') || '10'
+    );
+    const dispatchHandlerClientPayloadData = inputNotRequired(
+      'dispatch-handler-client-payload-data'
+    );
 
     if (dispatchHandlerConfig) {
       core.startGroup('Dispatch Handler Feature - Handle');
-      await handle(dispatchHandlerToken, dispatchHandlerConfig, dispatchHandlerMax);
+      await handle(
+        dispatchHandlerToken,
+        dispatchHandlerConfig,
+        dispatchHandlerMax
+      );
       core.endGroup();
     } else if (dispatchHandlerClientPayloadData) {
       core.startGroup('Dispatch Handler Feature - Dispatch');
-      const data: ClientPayloadData =  JSON.parse(dispatchHandlerClientPayloadData);
+      const data: ClientPayloadData = JSON.parse(
+        dispatchHandlerClientPayloadData
+      );
       await handleRepositoryDispatch(
         dispatchHandlerToken,
         dispatchHandlerOwner,
@@ -97,7 +127,7 @@ async function run() {
       core.endGroup();
     }
   } catch (error) {
-    core.debug(inspect(error))
+    core.debug(inspect(error));
     core.setFailed(error.message);
   }
 }
--- src/dispatch-handler.ts
+++ src/dispatch-handler.ts
@@ -1,29 +1,34 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 import jexl from 'jexl';
-import {inspect} from 'util'
+import {inspect} from 'util';
 
-
-export async function handle(token: string, config: string, max: number): Promise<void> {
+export async function handle(
+  token: string,
+  config: string,
+  max: number
+): Promise<void> {
   core.debug(`github context: ${inspect(github.context, true, 10)}`);
 
   const context = getCurrentContext();
   const data = getCurrentData();
-  core.info(`Current zoo context:\n ${inspect(context, true, 10)}`)
+  core.info(`Current zoo context:\n ${inspect(context, true, 10)}`);
   core.debug(`data: ${inspect(data)}`);
 
   if (context.handler_count && context.handler_count > max) {
-    throw new Error(`Max handler count ${max} reached, do you have a dispatch loop?`);
+    throw new Error(
+      `Max handler count ${max} reached, do you have a dispatch loop?`
+    );
   }
 
   const configs = getHandlerConfigsFromJson(config);
   core.debug(`configs: ${inspect(configs)}`);
-  let matchConfig: HandlerConfig|undefined;
+  let matchConfig: HandlerConfig | undefined;
   const expressionContext: ExpressionContext = {
     initial: context.handler_count === 0,
     context: context,
     data: data
-  }
+  };
   for (const config of configs) {
     core.debug(`config: ${inspect(config, true, 10)}`);
     if (config.if) {
@@ -44,24 +49,38 @@ export async function handle(token: string, config: string, max: number): Promis
     build_zoo_handler_data: {}
   };
 
-  core.info(`New zoo context:\n ${inspect(p.build_zoo_handler_context, true, 10)}`)
+  core.info(
+    `New zoo context:\n ${inspect(p.build_zoo_handler_context, true, 10)}`
+  );
 
   if (matchConfig) {
-    core.info(`Match config to use for dispatch\n ${inspect(matchConfig, true, 10)}`);
-    await sendRepositoryDispatch(token, matchConfig.owner, matchConfig.repo, matchConfig.event_type, p);
+    core.info(
+      `Match config to use for dispatch\n ${inspect(matchConfig, true, 10)}`
+    );
+    await sendRepositoryDispatch(
+      token,
+      matchConfig.owner,
+      matchConfig.repo,
+      matchConfig.event_type,
+      p
+    );
   } else {
     core.info('Nothing to do, bye bye');
   }
 }
 
-async function evaluate(expression: string, context: ExpressionContext): Promise<boolean> {
-  core.debug(`evaluating: expr ${expression} context ${inspect(context, true, 10)}`);
+async function evaluate(
+  expression: string,
+  context: ExpressionContext
+): Promise<boolean> {
+  core.debug(
+    `evaluating: expr ${expression} context ${inspect(context, true, 10)}`
+  );
   try {
     const ret = await jexl.eval(expression, context);
     core.debug(`evaluation result ${ret}`);
     return ret === true;
-  } catch (error) {
-  }
+  } catch (error) {}
   return false;
 }
 
@@ -72,10 +91,16 @@ export async function handleRepositoryDispatch(
   eventType: string,
   clientPayloadData: ClientPayloadData
 ) {
-  if (clientPayloadData.owner === undefined && github.context.payload.repository) {
+  if (
+    clientPayloadData.owner === undefined &&
+    github.context.payload.repository
+  ) {
     clientPayloadData.owner = github.context.payload.repository.owner.login;
   }
-  if (clientPayloadData.repo === undefined && github.context.payload.repository) {
+  if (
+    clientPayloadData.repo === undefined &&
+    github.context.payload.repository
+  ) {
     clientPayloadData.repo = github.context.payload.repository.name;
   }
   if (clientPayloadData.properties === undefined) {
@@ -86,7 +111,7 @@ export async function handleRepositoryDispatch(
     clientPayloadData.properties[key] = props[key];
   }
   const context = getCurrentContext();
-  core.info(`Current zoo context:\n ${inspect(context, true, 10)}`)
+  core.info(`Current zoo context:\n ${inspect(context, true, 10)}`);
   core.info('Prepare to send repository dispatch');
   core.debug(`github context: ${inspect(github.context, true, 10)}`);
   const p: ClientPayload = {
@@ -107,8 +132,20 @@ export async function sendRepositoryDispatch(
   eventType: string,
   clientPayload: any
 ) {
-  core.info(`Repository dispatch owner=${owner} repo=${repo} event_type=${eventType} payload=\n${inspect(clientPayload, true, 10)}`);
-  core.debug(`Sending repository dispatch ${owner} ${repo} ${eventType} ${inspect(clientPayload, true, 10)}`)
+  core.info(
+    `Repository dispatch owner=${owner} repo=${repo} event_type=${eventType} payload=\n${inspect(
+      clientPayload,
+      true,
+      10
+    )}`
+  );
+  core.debug(
+    `Sending repository dispatch ${owner} ${repo} ${eventType} ${inspect(
+      clientPayload,
+      true,
+      10
+    )}`
+  );
   const octokit = github.getOctokit(token);
   await octokit.repos.createDispatchEvent({
     owner: owner,
@@ -121,11 +158,12 @@ export async function sendRepositoryDispatch(
 export async function extractContextProperties(): Promise<void> {
   let count = 0;
   try {
-    const props = github.context.payload.client_payload.build_zoo_handler_context.properties as {[key: string]: string;};
+    const props = github.context.payload.client_payload
+      .build_zoo_handler_context.properties as {[key: string]: string};
     for (let key in props) {
       const value = props[key];
       core.exportVariable(`BUILD_ZOO_HANDLER_${key}`, value);
-      core.debug(`Extract context property ${key} ${value}`)
+      core.debug(`Extract context property ${key} ${value}`);
       count++;
     }
   } catch (error) {
@@ -137,11 +175,11 @@ export async function extractContextProperties(): Promise<void> {
 
 const TOKEN_PREFIX: RegExp = /^BUILD_ZOO_HANDLER.*$/;
 
-function readContextProperties(): {[key: string]: string;} {
-  const props: {[key: string]: string;} = {}
+function readContextProperties(): {[key: string]: string} {
+  const props: {[key: string]: string} = {};
   Object.keys(process.env)
-    .filter((env) => env.match(TOKEN_PREFIX))
-    .forEach((env) => {
+    .filter(env => env.match(TOKEN_PREFIX))
+    .forEach(env => {
       const key = env.replace('BUILD_ZOO_HANDLER_', '');
       const value = process.env[env] || '';
       props[key] = value;
@@ -150,7 +188,10 @@ function readContextProperties(): {[key: string]: string;} {
 }
 
 function getCurrentContext(): ClientPayloadContext {
-  if (github.context.payload.client_payload && github.context.payload.client_payload.build_zoo_handler_context) {
+  if (
+    github.context.payload.client_payload &&
+    github.context.payload.client_payload.build_zoo_handler_context
+  ) {
     return github.context.payload.client_payload.build_zoo_handler_context;
   } else {
     return {handler_count: 0, properties: {}};
@@ -158,7 +199,10 @@ function getCurrentContext(): ClientPayloadContext {
 }
 
 function getCurrentData(): ClientPayloadData {
-  if (github.context.payload.client_payload && github.context.payload.client_payload.build_zoo_handler_data) {
+  if (
+    github.context.payload.client_payload &&
+    github.context.payload.client_payload.build_zoo_handler_data
+  ) {
     return github.context.payload.client_payload.build_zoo_handler_data;
   } else {
     return {};
@@ -167,7 +211,7 @@ function getCurrentData(): ClientPayloadData {
 
 export interface ClientPayloadContext {
   handler_count: number;
-  properties: {[key: string]: string;}
+  properties: {[key: string]: string};
 }
 
 export interface ClientPayloadData {
@@ -175,7 +219,7 @@ export interface ClientPayloadData {
   owner?: string;
   repo?: string;
   [key: string]: any;
-  properties?: {[key: string]: string;}
+  properties?: {[key: string]: string};
 }
 
 export interface ClientPayload {
@@ -196,20 +240,19 @@ interface HandlerConfig {
   event_type: string;
 }
 
-
 export function getHandlerConfigsFromJson(json: string): HandlerConfig[] {
   const jsonConfig = JSON.parse(json);
   core.debug(`JSON config: ${inspect(jsonConfig)}`);
 
-  const configs: HandlerConfig[] = []
+  const configs: HandlerConfig[] = [];
   for (const jc of jsonConfig) {
     const config: HandlerConfig = {
       if: jc.if,
       owner: jc.owner,
       repo: jc.repo,
       event_type: jc.event_type ? jc.event_type : 'build-zoo-handler'
-    }
-    configs.push(config)
+    };
+    configs.push(config);
   }
   return configs;
 }
